b05fb521f2290d8d5f4e680e79b48f958014eaf9
stathis <stathischaritos@gmail.com>
2016-04-24 16:59:51 +0200

    relations tests

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ README:
 
 This is a small RESTful web service that handles TODO lists, tasks and tags, made with Django, the Django Rest Framework module, using Swagger for documentation and Behave for testing.
 
-I decided to go with Django as it was in the assignment's "prefered" list and I though it would be a good opportunity to gain some Django experiece. I cannot say I didn't regret my decision several times throughout this small project, but I do see the appeal of Django after delving into it for about a week, and I will continue to use it for certain types of projects.
+Running:
+=======
 
-NOTES:
-======
+    pip install -r requirements.txt
+    
+    python managepy runserver
+    
+    open http://localhost:8000/api/docs
 
-Django has a very strict way of doing things which could be valuable in very large projects. The rest framework and the automated swagger docs are a big plus and essential to serious web services.
+Testing
+=======
 
-Relations with django were a big hastle (most of the time spent for the nested writable many-to-many relation between tasks and tags). Part of it may be due to my inexperience with the framework, but I am positive that these simple relations would not have taken so much time to implement in NodeJS-Express-Mongo - even for a beginner.
+    python manage.py test api
--- src/api/features/steps/todoRelationsTest.py
+++ src/api/features/steps/todoRelationsTest.py
@@ -15,6 +15,15 @@ params={
     'format':'json'
 }
 
+taskData = {
+    'title': 'Test task title',
+    'details': 'Test task details',
+    'parentList': "10",
+    'tags' : ['testTag1,testTag2']
+}
+
+API_URL = "http://127.0.0.1:8000/api/"
+
 @given('the web service is running')
 def step_impl(context):
     pass
@@ -22,7 +31,7 @@ def step_impl(context):
 @when('I do a POST request to the api/lists endpoint with a new todo list object')
 def step_impl(context):
     context.model = 'lists'
-    response = requests.post('http://localhost:8000/api/lists/', json=listData, headers=headers, params=params )
+    response = requests.post(API_URL+'lists/', json=listData, headers=headers, params=params )
     assert response.status_code == 201
     context.response = response.json()
 
@@ -38,7 +47,7 @@ def step_impl(context):
 
 @then('it should be returned when doing a GET request to the api/lists endpoint followed by that specific list`s name')
 def step_impl(context):
-    response = requests.get('http://localhost:8000/api/lists/' + str(context.response['id']), headers=headers, params=params )
+    response = requests.get(API_URL+'lists/' + str(context.response['id']), headers=headers, params=params )
     assert response.status_code == 200
     todoList = response.json()
     assert todoList['name'] == listData['name']
@@ -47,32 +56,44 @@ def step_impl(context):
     
 @then('it should be inside the list of results when doing a GET request to the api/lists endpoint')
 def step_impl(context):
-    response = requests.get('http://localhost:8000/api/lists', headers=headers, params=params )
+    response = requests.get(API_URL+'lists', headers=headers, params=params )
     assert response.status_code == 200
     assert any(x['id'] == context.response['id'] for x in response.json())
 
-taskData = {
-    'title': 'Test task title',
-    'details': 'Test task details',
-    'parentList': None,
-    'tags' : ['testTag1,testTag2']
-}
 
 @given('a todo list has been created')
 def step_impl(context):
     context.model = 'lists'
-    response = requests.post('http://localhost:8000/api/lists/', json=listData, headers=headers, params=params )
+    response = requests.post(API_URL+'lists/', json=listData, headers=headers, params=params )
     assert response.status_code == 201
     context.response = response.json()
 
+@given('a todo task has been created')
+def step_impl(context):
+    context.model = 'tasks'
+    taskData['parentList'] = "10"
+    response = requests.post(API_URL+'tasks/', json=taskData, headers=headers, params=params )
+    assert response.status_code == 201
+    context.response = response.json()
+    
 @when('I create new tasks for that list')
 def step_impl(context):
     taskData['parentList'] = context.response['id']
-    requests.post('http://localhost:8000/api/tasks/', json=taskData, headers=headers, params=params )
+    requests.post(API_URL+'tasks/', json=taskData, headers=headers, params=params )
 
+@when('I update the task with new tags')
+def step_impl(context):
+    taskData['tags'] = ['testTag3,testTag4']
+    requests.put(API_URL+'tasks/' + str(context.response['id']) , json=taskData, headers=headers, params=params )
+
+@then('the tags list should be updated')
+def step_impl(context):
+    response = requests.get(API_URL+'tags/', headers=headers, params=params )
+    assert len(response.json())
+    
 @then('these tasks should appear nested inside it')
 def step_impl(context):
-    response = requests.get('http://localhost:8000/api/lists/' + str(context.response['id']), headers=headers, params=params )
+    response = requests.get(API_URL+'lists/' + str(context.response['id']), headers=headers, params=params )
     assert response.status_code == 200
     todoList = response.json()
     assert todoList['name'] == listData['name']
@@ -81,15 +102,15 @@ def step_impl(context):
 
 @when('I delete that todo list')
 def step_impl(context):
-    requests.delete('http://localhost:8000/api/lists/' + str(context.response['id']))
+    requests.delete(API_URL+'lists/' + str(context.response['id']))
 
 @then('the list should not appear in the api/lists list')
 def step_impl(context):
-    response = requests.get('http://localhost:8000/api/lists', headers=headers, params=params )
+    response = requests.get(API_URL+'lists', headers=headers, params=params )
     assert not any(x['id'] == context.response['id'] for x in response.json())
 
 @then('all the tasks of that list should be deleted')
 def step_impl(context):
-    response = requests.get('http://localhost:8000/api/tasks', headers=headers, params=params )
+    response = requests.get(API_URL+'tasks', headers=headers, params=params )
     assert response.status_code == 200
     assert not any(x['parentList'] == context.response['id'] for x in response.json())
